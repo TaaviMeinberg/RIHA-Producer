ad12150a4b2bd427c0cf0b762a2bbc162b0ff142
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-04-12 11:16:00 +0300

    Finished re-factoring and fixed the issues
    Issues was caused by calling the script before the form that it was
    reading from. Original code was correct, so I restored back to that,
    only change is script call in form.html is now after HTML code.

--- src/main/resources/static/js/CancelButton.js
+++ src/main/resources/static/js/CancelButton.js
@@ -1,16 +1,14 @@
-
 var nameOnLoad = document.forms["infosystem-form"]["name"].value;
 var shortNameOnLoad = getInputValue("infosystem-form", "shortName");
 var documentationOnLoad = getInputValue("infosystem-form", "documentation");
 var objectiveOnLoad = getInputValue("infosystem-form", "objective");
 
-console.log(nameOnLoad);
 function cancel() {
 	var name = getInputValue("infosystem-form", "name");
 	var shortName = getInputValue("infosystem-form", "shortName");
 	var documentation = getInputValue("infosystem-form", "documentation");
 	var objective = getInputValue("infosystem-form", "objective");
-	console.log(nameOnLoad);
+	
 	if ((name == "" || name == nameOnLoad)
 			&& (shortName == "" || shortName == shortNameOnLoad)
 			&& (documentation == "" || documentation == documentationOnLoad)
