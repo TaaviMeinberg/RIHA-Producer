2d484a05539a9a1a8d2ffd4537e43d97c1b935d8
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-04-12 10:03:03 +0300

    Re-factored existing script into a new .js file.
    Created a new .js file and moved Cancel() function into it.

---
+++ src/main/resources/static/js/CancelButton.js
@@ -0,0 +1,26 @@
+var nameOnLoad = getInputValue("infosystem-form", "name");
+var shortNameOnLoad = getInputValue("infosystem-form", "shortName");
+var documentationOnLoad = getInputValue("infosystem-form", "documentation");
+var objectiveOnLoad = getInputValue("infosystem-form", "objective");
+
+function cancel() {
+	var name = getInputValue("infosystem-form", "name");
+	var shortName = getInputValue("infosystem-form", "shortName");
+	var documentation = getInputValue("infosystem-form", "documentation");
+	var objective = getInputValue("infosystem-form", "objective");
+
+	if ((name == "" || name == nameOnLoad)
+			&& (shortName == "" || shortName == shortNameOnLoad)
+			&& (documentation == "" || documentation == documentationOnLoad)
+			&& (objective == "" || objective == objectiveOnLoad)) {
+		window.location = "/";
+	} else {
+		if (confirm("Kas kindlasti soovid katkestada?")) {
+			window.location = "/";
+		}
+	}
+}
+
+function getInputValue(formName, inputName) {
+	return document.forms[formName][inputName].value;
+}
