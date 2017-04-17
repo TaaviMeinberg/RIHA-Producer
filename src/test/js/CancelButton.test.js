154411097b9a56b115a446060b827b59d716aa10
Taavi Meinberg <taavi.meinberg@gmail.com>
2017-04-17 21:53:05 +0300

    Removed a variable and trying to get constructor working

--- src/test/js/CancelButton.test.js
+++ src/test/js/CancelButton.test.js
@@ -1,12 +1,12 @@
 describe('CancelButton', function() {
 	it('Cancel button click takes back to index', function() {
 		loadFixtures('form.html');
-		var CancelButton = new CancelButton();
-		spyOn(CancelButton, '_redirect');
-		CancelButton._initCancelButton();
+		//var button = new CancelButton();
+		spyOn(new CancelButton(),'_redirect');
+		new CancelButton()._initCancelButton();
 
 		$('#cancelButton').trigger('click');
 
-		expect(CancelButton._redirect).toHaveBeenCalledWith('/');
+		expect(new CancelButton()._redirect).toHaveBeenCalledWith('/');
 	});
 });
