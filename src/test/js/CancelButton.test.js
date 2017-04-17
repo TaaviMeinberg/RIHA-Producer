8361f9618bcc24d7b3f2542eca4d4b01b345e0aa
Taavi Meinberg <taavi.meinberg@gmail.com>
2017-04-17 21:57:18 +0300

    Moved spyOn() into expect()

--- src/test/js/CancelButton.test.js
+++ src/test/js/CancelButton.test.js
@@ -2,11 +2,10 @@ describe('CancelButton', function() {
 	it('Cancel button click takes back to index', function() {
 		loadFixtures('form.html');
 		//var button = new CancelButton();
-		spyOn(new CancelButton(),'_redirect');
 		new CancelButton()._initCancelButton();
 
 		$('#cancelButton').trigger('click');
 
-		expect(new CancelButton()._redirect).toHaveBeenCalledWith('/');
+		expect(spyOn(new CancelButton(),'_redirect')).toHaveBeenCalledWith('/');
 	});
 });
