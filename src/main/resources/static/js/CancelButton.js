18cd2c426620dc68d35ae6b8ef2c739994f7efb8
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-04-17 14:24:56 +0300

    Added all if conditions.

--- src/main/resources/static/js/CancelButton.js
+++ src/main/resources/static/js/CancelButton.js
@@ -11,22 +11,35 @@ function CancelButton() {
 	self._redirect = function(url) {
 		window.location = url;
 	};
-	
-	self._getInputValue = function (inputID) {
+
+	self._getInputValue = function(inputID) {
 		console.log(document.getElementById(inputID).value);
-	    return document.getElementById(inputID).value;
-	  };
+		return document.getElementById(inputID).value;
+	};
+
+	var nameOnLoad = self._getInputValue("name");
+	var shortNameOnLoad = self._getInputValue("short-name");
+	var documentationOnLoad = self._getInputValue("documentation");
+	var objectiveOnLoad = self._getInputValue("objective");
 
 	self._initCancelButton = function() {
-		$('#cancelButton').on('click', function() {
-			if(self._getInputValue("name")=="" || self._getInputValue("short-name")==""){
-				self._redirect('/');
-			} else {
-				if(confirm("Kas kindlasti soovid katkestada?")){
-					self._redirect('/');
-				}
-			}
-			
-		});
+		$('#cancelButton').on('click',function() {
+							var name = self._getInputValue("name");
+							var shortName = self._getInputValue("short-name");
+							var documentation = self._getInputValue("documentation");
+							var objective = self._getInputValue("objective");
+
+							if ((name == "" || name == nameOnLoad)
+									&& (shortName == "" || shortName == shortNameOnLoad)
+									&& (documentation == "" || documentation == documentationOnLoad)
+									&& (objective == "" || objective == objectiveOnLoad)) {
+								self._redirect('/');
+							} else {
+								if (confirm("Kas kindlasti soovid katkestada?")) {
+									self._redirect('/');
+								}
+							}
+
+						});
 	};
 }
