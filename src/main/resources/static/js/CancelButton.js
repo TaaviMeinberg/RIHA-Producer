0dcef2540994cba7d59de684c13a1872b0a6cb2b
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-04-17 12:31:15 +0300

    added phantomJS functions to hopefully make it phantomJS friendly.

--- src/main/resources/static/js/CancelButton.js
+++ src/main/resources/static/js/CancelButton.js
@@ -1,16 +1,37 @@
 "use strict";
 
-var nameOnLoad = getInputValue("name");
-var shortNameOnLoad = getInputValue("short-name");
-var documentationOnLoad = getInputValue("documentation");
-var objectiveOnLoad = getInputValue("objective");
+var webPage = require('webpage');
+var page = webPage.create();
+
+page.open('/Users/taavim/Documents/workspace/RIHA-Producer/src/main/resources/templates/form.html', function(status) {
+
+var nameOnLoad = page.evaluate(function() {
+	return document.getElementById("name").value;
+});
+var shortNameOnLoad = page.evaluate(function() {
+	return document.getElementById("short-name").value;
+});
+var documentationOnLoad = page.evaluate(function() {
+	return document.getElementById("documentation").value;
+});
+var objectiveOnLoad = page.evaluate(function() {
+	return document.getElementById("objective").value;
+});
 
 function cancel() {
-	var name = getInputValue("name");
-	var shortName = getInputValue("short-name");
-	var documentation = getInputValue("documentation");
-	var objective = getInputValue("objective");
-	
+	var name = page.evaluate(function() {
+		return document.getElementById("name").value;
+	});
+	var shortName = page.evaluate(function() {
+		return document.getElementById("short-name").value;
+	});
+	var documentation = page.evaluate(function() {
+		return document.getElementById("documentation").value;
+	});
+	var objective = page.evaluate(function() {
+		return document.getElementById("objective").value;
+	});
+
 	if ((name == "" || name == nameOnLoad)
 			&& (shortName == "" || shortName == shortNameOnLoad)
 			&& (documentation == "" || documentation == documentationOnLoad)
@@ -22,7 +43,8 @@ function cancel() {
 		}
 	}
 }
-
-function getInputValue(inputID) {
-	return document.getElementById(inputID).value;
-}
+});
+/*
+ * function getInputValue(inputID) { return
+ * document.getElementById(inputID).value; }
+ */
