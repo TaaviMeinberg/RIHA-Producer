bd7ba18d0b7eb8ca76f0122960d9a0ed784f7b8e
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-04-18 10:47:07 +0300

    Removed calls to CancelButton class.

--- src/test/js/CancelButton.test.js
+++ src/test/js/CancelButton.test.js
@@ -1,12 +1,9 @@
 describe('CancelButton', function() {
 	it('Cancel button click takes back to index', function() {
 		loadFixtures('form.html');
-		//var button = new CancelButton();
-		new CancelButton()._initCancelButton();
-
 		$('#cancelButton').trigger('click');
-		
+
 		expect('click').toHaveBeenTriggeredOn('#cancelButton');
-		expect(spyOn(new CancelButton(),'_redirect')).toHaveBeenCalledWith('/');
+		expect(spyOn(button, '_redirect')).toHaveBeenCalledWith('/');
 	});
 });
