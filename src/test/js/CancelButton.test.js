2adc63f0bcde19ead762a251ecfa683b57641f48
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-04-18 10:55:16 +0300

    Fixed reference error.

--- src/test/js/CancelButton.test.js
+++ src/test/js/CancelButton.test.js
@@ -1,8 +1,10 @@
 describe('CancelButton', function() {
 	it('Cancel button click takes back to index', function() {
 		loadFixtures('form.html');
+		
+		var button = new CancelButton(); 
 		$('#cancelButton').trigger('click');
-
+		
 		expect('click').toHaveBeenTriggeredOn('#cancelButton');
 		expect(spyOn(button, '_redirect')).toHaveBeenCalledWith('/');
 	});
