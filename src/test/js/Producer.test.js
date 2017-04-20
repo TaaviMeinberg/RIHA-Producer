234c109293e0ef39f6052b8d88598e909ff82842
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-04-20 11:17:34 +0300

    Moved cancelButton test to producer.test file

--- src/test/js/Producer.test.js
+++ src/test/js/Producer.test.js
@@ -82,4 +82,15 @@ describe('Producer', function () {
 
     expect(producer._redirect).toHaveBeenCalledWith('/edit/lühinimi');
   });
+  
+  it('Cancel button click takes back to index', function() {
+		loadFixtures('form.html');
+		var producer = new CancelButton();
+	    spyOn(producer, '_redirect');
+	    producer._initCancelButton();
+
+	    $('#infosystem-form').find('button.edit').trigger('click');
+
+	    expect(producer._redirect).toHaveBeenCalledWith('/');
+	});
 });
